b99627bfb6f96ec3d808ee0aea8f2296dabdf15d
Hana_han <53941705+RobertHan96@users.noreply.github.com>
2021-08-22 02:57:42 +0900

    feat : AppDelegate 수정 -> IQKeyboardManager 설정

--- OfMe/AppDelegate.swift
+++ OfMe/AppDelegate.swift
@@ -1,5 +1,6 @@
- import UIKit
-
+import UIKit
+import IQKeyboardManager
+ 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -8,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Thread.sleep(forTimeInterval: 2.0)
+        IQKeyboardManager.shared().isEnabled = true
+        
         return true
     }
 
