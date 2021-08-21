a6f91d6c58b3123e666c935cb255708ad5898014
Hana_han <53941705+RobertHan96@users.noreply.github.com>
2021-08-22 00:46:28 +0900

    feat : 프로젝트 폴더 구조 추가, Main->Cell 추가

---
+++ OfMe/Home/Main/Cells/TimeInfoCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimeInfoCell.swift
+//  OfMe
+//
+//  Created by HanaHan on 2021/08/22.
+//
+
+import UIKit
+
+class TimeInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
