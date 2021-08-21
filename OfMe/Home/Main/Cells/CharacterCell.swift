75d96c837195dd8e7328017123665a63614224ce
Hana_han <53941705+RobertHan96@users.noreply.github.com>
2021-08-22 01:04:21 +0900

    feat: Main-Timeinfoell 추가

---
+++ OfMe/Home/Main/Cells/CharacterCell.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterCell.swift
+//  OfMe
+//
+//  Created by HanaHan on 2021/08/22.
+//
+
+import UIKit
+
+class CharacterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
