bb28203b6c478dff8633666452fbf5cad7f9be61
Hana_han <53941705+RobertHan96@users.noreply.github.com>
2021-09-06 21:22:26 +0900

    feat: 로그인 관련 파일 정리

---
+++ OfMe/Login/SocialLoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SocialLoginViewController.swift
+//  OfMe
+//
+//  Created by HanaHan on 2021/09/06.
+//
+
+import UIKit
+
+class SocialLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    private func setupUI() {
+        
+    }
+
+    private func fetchData() {
+        
+    }
+
+}
