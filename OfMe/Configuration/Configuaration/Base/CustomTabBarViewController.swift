b47b70899bffde3669f658002c3bdc7ff206757f
Hana_han <53941705+RobertHan96@users.noreply.github.com>
2021-09-09 13:10:09 +0900

    fix: 2번째 탭바 아이콘 안보여지는 오류 임시수정

--- OfMe/Configuration/Configuaration/Base/CustomTabBarViewController.swift
+++ OfMe/Configuration/Configuaration/Base/CustomTabBarViewController.swift
@@ -2,14 +2,14 @@ import UIKit
 
 class CustomTabBarViewController: UITabBarController, UITabBarControllerDelegate {
     let data = Date()
-    
+    let image = UIImage(named: "new_test_icon")
     let homeVC = HomeMainViewController()
     let questionVC = QuestionViewController()
-    let archiveVC = ArchiveMainViewController()
+    let tesetVC = TestConceptMainViewController()
     let myinfoVC = MyInfoMainViewController()
     let archiveWriteVC = ArchiveWriteViewController()
     let homeTabBarItem = UITabBarItem(title: nil, image: UIImage(named: ImgName.imgName(of: .homeIcon)), tag: 0)
-    let archiveTabBarItem = UITabBarItem(title: nil, image: UIImage(named: ImgName.imgName(of: .testIcon)), tag: 1)
+    let tesetTabBarItem = UITabBarItem(title: nil, image: UIImage(named: "new_test_icon"), tag: 1)
     let questionTabBarItem = UITabBarItem(title: nil, image: UIImage(named: ImgName.imgName(of: .questionIcon)), tag: 2)
     let myinfoTabBarItem = UITabBarItem(title: nil, image: UIImage(named: ImgName.imgName(of: .defaultProfile)), tag: 3)
 
@@ -24,15 +24,19 @@ class CustomTabBarViewController: UITabBarController, UITabBarControllerDelegate
             return tabBar
         }()
         self.setValue(tabBar, forKey: "tabBar")
-        
+        tesetTabBarItem.badgeColor = .black
+        self.tabBarController?.updateTabBarItem(tab: 1, image: image)
+        tesetTabBarItem.image = image?.withRenderingMode(.alwaysOriginal)
+        tesetTabBarItem.selectedImage = image
+
         let homeNavVC = UINavigationController(rootViewController: homeVC)
         let questionNavVC = UINavigationController(rootViewController: questionVC)
-        let archiveNavVC = UINavigationController(rootViewController: archiveVC)
+        let archiveNavVC = UINavigationController(rootViewController: tesetVC)
         let myinfoNavVC = UINavigationController(rootViewController: myinfoVC)
-        
+
         homeNavVC.tabBarItem = homeTabBarItem
         questionNavVC.tabBarItem = questionTabBarItem
-        archiveNavVC.tabBarItem = archiveTabBarItem
+        tesetVC.tabBarItem = tesetTabBarItem
         myinfoNavVC.tabBarItem = myinfoTabBarItem
         
         self.viewControllers = [
@@ -50,6 +54,21 @@ class CustomTabBarViewController: UITabBarController, UITabBarControllerDelegate
         tabBar.items?[1].titlePositionAdjustment.horizontal = -15
         tabBar.items?[2].titlePositionAdjustment.horizontal = 15
         tabBar.items?[3].titlePositionAdjustment.horizontal = -15
+    }
+
+}
+
+extension UITabBarController {
+
+    func updateTabBarItem(tab: Int, image: UIImage?) {
+
+        guard let tabItems = tabBar.items, tab < tabItems.count && tab >= 0
+            else { return }
+
+        let tabItem = tabItems[tab]
+        tabItem.image = image?.withRenderingMode(.alwaysOriginal)
+        tabItem.selectedImage = tabItem.image
+
     }
 
 }
